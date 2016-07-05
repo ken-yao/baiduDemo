be8bf226214a19dc3d99634260719ffba4c2266c
ken yaos <1013005839@qq.com>
2016-07-06 01:07:43 +0800

    '20160706'

--- Page/IndexPage.js
+++ Page/IndexPage.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 
-import BaiduMap from 'baidumapkit';
+import MapView from 'react-native-baidumap';
+import KKLocation from 'react-native-baidumap/KKLocation';
 
 export default class IndexPage extends Component {
+
+  componentDidMount() {
+    this.refs["mapView"].zoomToLocs([[39.918541, 116.4835]]);
+    KKLocation.getCurrentPosition((position) => {
+        console.log("location get current position: ", position);
+    }, (error) => {
+        console.log("location get current position error: ", error);
+    });
+    this.watchID = KKLocation.watchPosition((position) => {
+        console.log("watch position: ", position);
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          This is the Index Page
-        </Text>
-      </View>
+      <MapView
+        style={{flex: 1}}
+        ref="mapView"
+        showsUserLocation={true}
+        userLocationViewParams={{accuracyCircleFillColor: 'red', image: require('./start_icon.png')}}
+        annotations={[{latitude: 39.832136, longitude: 116.34095, title: "start", subtile: "hello", image: require('./amap_start.png')}, {latitude: 39.902136, longitude: 116.44095, title: "end", subtile: "hello", image: require('./amap_end.png')}]}
+        overlays={[{coordinates: [{latitude: 39.832136, longitude: 116.34095}, {latitude: 39.832136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.44095}], strokeColor: '#666666', lineWidth: 3}]}
+      />
     );
   }
 }
--- android/app/src/main/java/com/baidudemo/MainActivity.java
+++ android/app/src/main/java/com/baidudemo/MainActivity.java
@@ -4,53 +4,39 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+import com.yiyang.reactnativebaidumap.ReactMapPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
-import com.bee.baidumapview.BaiduMapModuleReactPackage;
 
 public class MainActivity extends ReactActivity {
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "baiduDemo";
+    }
 
-  @Override
-  protected String getMainComponentName() {
-      return "baiduDemo";
-  }
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
 
-  @Override
-  protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-  }
-
-  @Override
-  protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new BaiduMapModuleReactPackage(),
-          new BaiduMapReactPackage(this),
-      );
-  }
-
-
-  private ReactInstanceManager mReactInstanceManager;
-  private ReactRootView mReactRootView;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      //初始化百度地图
-      SDKInitializer.initialize(getApplicationContext());
-
-      mReactRootView = new ReactRootView(this);
-      mReactInstanceManager = ReactInstanceManager.builder()
-              .setApplication(getApplication())
-              .setBundleAssetName("index.android.bundle")
-              .setJSMainModuleName("index.android")
-              .addPackage(new MainReactPackage())
-              .addPackage(new BaiduMapReactPackage(this)) // <-- Register package here
-              .setUseDeveloperSupport(true)
-              .setInitialLifecycleState(LifecycleState.RESUMED)
-              .build();
-
-      mReactRootView.startReactApplication(mReactInstanceManager, "AwesomeProj", null);
-      setContentView(mReactRootView);
-  }
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(), new ReactMapPackage() // <---- and This!
+        );
+    }
 }
